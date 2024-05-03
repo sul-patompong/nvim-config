c7c398dbad2043e681826449e8f86b9f62fdd6c4
Patompong Sulsaksakul <patompong.beam@gmail.com>
2024-05-03 17:34:14 +0700

    change color scheme to catppuccin, add sql-formatter

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,11 @@
 return {
   { "savq/melange-nvim" },
 
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "melange",
+      colorscheme = "catppuccin-mocha",
     },
   },
 }
