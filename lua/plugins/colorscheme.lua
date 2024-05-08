7892053295b29507a0f8030c1b7a96e183c20d31
Patompong Sulsaksakul <patompong.beam@gmail.com>
2024-05-08 20:29:28 +0700

    use default theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,9 @@
 return {
-  { "savq/melange-nvim" },
-
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin-macchiato",
-    },
-  },
+  -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "catppuccin-macchiato",
+  --   },
+  -- },
 }
