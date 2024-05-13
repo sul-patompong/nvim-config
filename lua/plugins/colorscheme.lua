f87de5cd10f871162fb9c5b4c1eacc9662556c77
Patompong Sulsaksakul <patompong.beam@gmail.com>
2024-05-13 22:17:19 +0700

    make it use 'tokyonight' theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,11 @@
 return {
+  -- { "npxbr/gruvbox.nvim", name = "gruvbox", priority = 1000 },
+  -- { "savq/melange-nvim" },
   -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  -- {
-  --   "LazyVim/LazyVim",
-  --   opts = {
-  --     colorscheme = "catppuccin-macchiato",
-  --   },
-  -- },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
 }
